749eda9c9bde0343debceb68ca342b74b617c39a
ShaliniSF79 <shalini.suresh@syncfusion.com>
2025-11-13 16:35:28 +0530

    sample included

---
+++ LiveDataSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LiveDataSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LiveDataSample/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Microsoft.Maui.Dispatching;
+
+namespace LiveDataSample
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+    }
+}
